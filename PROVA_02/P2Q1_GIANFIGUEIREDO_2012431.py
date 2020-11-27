06b7e6b5a153bb4b6ae9d30a9e554fb909cc79f7
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2020-11-27 17:04:35 -0300

    - P2

--- PROVA_02/P2Q1_GIANFIGUEIREDO_2012431.py
+++ PROVA_02/P2Q1_GIANFIGUEIREDO_2012431.py
@@ -1,3 +1,4 @@
 #Nome completo: GIAN LUCA DA SILVA FIGUEIREDO
 #Matrícula PUC-Rio: 2012431
 #Declaração de autoria: declaro que este documento foi produzido em sua totalidade por mim, sem consultas a outros alunos, professores ou qualquer outra pessoa.
+
